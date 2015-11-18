c930f0b57b116f87575fa9a72da5dc4b468cf4c7
Jon Tejada <tejjon@gmail.com>
2015-11-17 16:35:25 -0800

    script parses the json and inserts figure elements into index

--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
 var abbr; //only way to get abbr into global scope?
 
-$(document).ready( function () {
-	$.getJSON("photos.json", function(data){ //get json object of full photo data
-		abbr = data.abbr; //load abbreviations from json into abbr variable
-		$.each(data.photos,makeDiv); //for each photo in the array, run the makeDiv function
-	});
+$(document).ready(function() {
+    $.getJSON("photos.json", function(data) { //get json object of full photo data
+        abbr = data.abbr; //load abbreviations from json into abbr variable
+        $.each(data.photos, makeDiv); //for each photo in the array, run the makeDiv function
+    });
 });
 
-function makeDiv (index,obj) { //get the index number of the array and the object with photo info
-	var divId = 'pic' + index;  //crete a unique HTML id for the div, based on index value
-	var imgSrc = 'thumb/' + obj.src; //create the thumbnail url 
-	var fullSrc = 'full/' + obj.src; //create the full image url
-	//var stoneClassArray = obj.stone;
-	var stoneClassString = obj.stone.join(' ');
-	var colorClassString = obj.color.join(' ');
-	console.log(colorClassString);
-	 // $.each(obj.stone,function(key,val) {
-	 // 	//stoneClassArray.push(val);
-	 // 	stoneClassArray
-	 // });
-	//console.log(stoneClassArray);
-	//$.each(obj.stone,convertAbbr);
-	//console.log(abbr);
-}
-
-function convertAbbr (i,shortName) {
-	//
-	return abbr[shortName];
+function makeDiv(index, obj) { //get the index number of the array and the object with photo info
+    var divId = 'pic' + index; //crete a unique HTML id for the div, based on index value
+    var imgSrc = 'img/thumb/' + obj.src; //create the thumbnail url 
+    var fullSrc = 'img/full/' + obj.src; //create the full image url
+    var stoneString = '';
+    $.each(obj.stone, function(key, val) { //build stoneString 
+        if (stoneString === '') {
+            stoneString = abbr[val];
+        } else {
+            stoneString += ' & ' + abbr[val];
+        }
+    });
+    var colorString = '';
+    $.each(obj.color, function(key, val) { //build colorString
+        if (colorString === '') {
+            colorString = abbr[val];
+        } else {
+            colorString += ' & ' + abbr[val];
+        }
+    });
+    //combine all info to make a figure html element 
+   	var html = "<figure id='" + divId + "' class='isoitem'><a href='" + fullSrc + "'><img src='"+ imgSrc + "' width='264' height='264'><h4>" + stoneString + "<br>" + colorString + "</h4></a><figcaption>Stone: " + stoneString + ". Color: " + colorString + "</figcaption></figure>";
+   	$('.isotope').append(html); //append the new figure element to the isotope div
 }
